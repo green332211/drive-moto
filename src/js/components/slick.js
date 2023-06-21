0c7933f380866614f56e15d85a80d5e8ec7cf7bc
green332211 <lobachtolyan123@gmail.com>
2023-06-21 16:06:52 +0300

    added an adaptive banner

--- src/js/components/slick.js
+++ src/js/components/slick.js
@@ -3,5 +3,13 @@ $(function () {
     dots: true,
     prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="../../img/arrow-left.svg" alt="arrow-left"></button>',
     nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="../../img/arrow-right.svg" alt="arrow-right"></button>',
+    responsive: [
+      {
+        breakpoint: 969,
+        settings: {
+          arrows: false,
+        }
+      }
+    ]
   });
 });
