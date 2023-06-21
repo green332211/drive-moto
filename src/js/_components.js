78a586c79d3ceaa9fd0a384bd7ecc68f06b61f94
green332211 <lobachtolyan123@gmail.com>
2023-06-21 12:20:21 +0300

    made an adaptive header

--- src/js/_components.js
+++ src/js/_components.js
@@ -3,3 +3,4 @@ import './components/search';
 import './components/products';
 import './components/catalog';
 import './components/product-card';
+import './components/header';
---
+++ src/js/components/header.js
@@ -0,0 +1,5 @@
+$(function () {
+  $('.menu__btn').on('click', function () {
+    $('.menu-mobile__list').toggleClass('menu-mobile__list--active');
+  });
+});
