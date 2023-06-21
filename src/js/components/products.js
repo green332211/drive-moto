1dc4c083c8751186f5601f3afad775ec651350fc
green332211 <lobachtolyan123@gmail.com>
2023-06-21 18:52:39 +0300

    added an adaptive to the second-banner partial and product-popular partial

--- src/js/components/products.js
+++ src/js/components/products.js
@@ -8,5 +8,35 @@ $(function (){
     slidesToScroll: 1,
     prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="../../img/arrow-black-left.svg" alt="arrow-left"></button>',
     nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="../../img/arrow-black-right.svg" alt="arrow-right"></button>',
+    responsive: [
+      {
+        breakpoint: 1301,
+        settings: {
+          arrows: false,
+          dots: true,
+        }
+      },
+      {
+        breakpoint: 1201,
+        settings: {
+          slidesToShow: 3,
+          dots: true,
+        }
+      },
+      {
+        breakpoint: 870,
+        settings: {
+          slidesToShow: 2,
+          dots: true,
+        }
+      },
+      {
+        breakpoint: 590,
+        settings: {
+          slidesToShow: 1,
+          dots: true,
+        }
+      }
+    ]
   });
 })
